7a4220093fdf9df77d309bd18282deb549e01ecb
Arif Ullah <arifullah565360@gmail.com>
2021-02-02 19:27:01 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,18 @@
+//  javascript False 
+// 0 ;
+// "" ;
+// undefined
+
+
+
+let name= NaN;
+console.log(name);
+
+// const age = 4;
+
+if(name){
+    console.log(" it,s true ");
+}
+else{
+    console.log(" it,s false ");
+} 
