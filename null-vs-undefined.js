2f4dd2c256d7f1fd98069f31f11b5beaf98d3815
Arif Ullah <arifullah565360@gmail.com>
2021-02-02 20:41:08 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,18 @@
+// let pakhi; 
+// function add( num1 , num2){
+//     console.log(num1 + num2);
+//     return add;
+// }
+ 
+// function add ( num1 , num2){
+//     console.log(num1, num2);
+// }
+
+// var premik =  {name :"smart dadu ",phone:01710}
+// console.log(premik.gf);
+
+// const fun = undefined;
+// console.log(fun);
+
+let ages = [ 12, 52, 5] ;
+console.log(ages[22]);
