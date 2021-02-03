a17bc257d84cbda21ace6731b1eae0c1bd9f7742
Arif Ullah <arifullah565360@gmail.com>
2021-02-03 10:36:50 +0600

    double vs triple

---
+++ doubleEqual-vs-tripleEqual.js
@@ -0,0 +1,24 @@
+//  double Equal dile konta kon typar jinis aii ta check kore nah 
+//  double equal mani sodo value ta k check korbe 
+//  triple equal   mani hocce value and type 2 ta kei chack korbe .
+
+
+// const first = 1;
+// const second = true;
+// if(first == second){
+//     console.log("it's true ");
+// }
+// else{
+//     console.log("it's false");
+// }
+
+
+
+const first = 50;
+const second = 60;
+if(first == second){
+    console.log("it's true ");
+}
+else{
+    console.log("it's false");
+}
